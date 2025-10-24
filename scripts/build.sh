10c5b800e9c2282e4a23fba222fa8a64ea9f6c23
janssen-tiobe <118828444+janssen-tiobe@users.noreply.github.com>
2025-10-24 15:09:20 +0200

    Added disabling terminal popup in windows

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+export CGO_ENABLED=0
 
-GOOS=windows GOARCH=amd64 go build -o build/htmltox.exe main.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-H=windowsgui" -o build/htmltox.exe main.go
 
 GOOS=linux GOARCH=amd64 go build -o build/htmltox-linux-amd64 main.go
 GOOS=linux GOARCH=arm64 go build -o build/htmltox-linux-arm64 main.go
