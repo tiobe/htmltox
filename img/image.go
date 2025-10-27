4bc292e8054cc0b14644260fdfaabcda9a14bd3a
janssen <118828444+janssen-tiobe@users.noreply.github.com>
2025-10-27 10:29:21 +0100

    For img not passing a selector will screenshot the full page

--- img/image.go
+++ img/image.go
@@ -13,12 +13,16 @@ var Command = &cli.Command{
 	Usage: "Capture an image of a webpage using a Chromium (based) browser.",
 	Flags: []cli.Flag{
 		&cli.StringFlag{Name: "output", Value: "htmltox.png", Aliases: []string{"o"}, Usage: "Path to output the image file"},
-		&cli.StringFlag{Name: "selector", Required: true, Aliases: []string{"S"}, Usage: "HTML selector to define what to create an image for (eg. div.tqi-label)."},
+		&cli.StringFlag{Name: "selector", Aliases: []string{"S"}, Usage: "HTML selector to define what to create an image for (eg. div.tqi-label)."},
 		&cli.Float64Flag{Name: "scale", Value: 3.0, Aliases: []string{"s"}, Usage: "Device scale factor (1.0 = low, 2.0 = normal, 3.0 = high, 4.0 = ultra high)"},
 	},
 	Action: func(ctx context.Context, cmd *cli.Command) error {
 		actionFunc := func(buffer *[]byte) chromedp.Action {
-			return chromedp.Screenshot(cmd.String("selector"), buffer)
+			selector := cmd.String("selector")
+			if selector != "" {
+				return chromedp.Screenshot(cmd.String("selector"), buffer)
+			}
+			return chromedp.FullScreenshot(buffer, 100)
 		}
 		return shared.Run(cmd, actionFunc, "image", cmd.Float64("scale"))
 	},
