e01170c8ab2dc032fd80b76d7b2c9c9364c39007
janssen <118828444+janssen-tiobe@users.noreply.github.com>
2025-11-19 15:55:36 +0100

    Fixed ldflags not being passed in the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 export CGO_ENABLED=0
 
-LDFLAGS="-ldflags \"-X main.Version=$VERSION\""
+LDFLAGS="-X main.Version=$VERSION"
 
-GOOS=windows GOARCH=amd64 go build "$LDFLAGS" -o build/htmltox.exe cmd/htmltox/main.go
+GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o build/htmltox.exe cmd/htmltox/main.go
 
-GOOS=linux GOARCH=amd64 go build "$LDFLAGS" -o build/htmltox-linux-amd64 cmd/htmltox/main.go
-GOOS=linux GOARCH=arm64 go build "$LDFLAGS" -o build/htmltox-linux-arm64 cmd/htmltox/main.go
+GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o build/htmltox-linux-amd64 cmd/htmltox/main.go
+GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o build/htmltox-linux-arm64 cmd/htmltox/main.go
 
-GOOS=darwin GOARCH=amd64 go build "$LDFLAGS" -o build/htmltox-darwin-amd64 cmd/htmltox/main.go
-GOOS=darwin GOARCH=arm64 go build "$LDFLAGS" -o build/htmltox-darwin-arm64 cmd/htmltox/main.go
+GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o build/htmltox-darwin-amd64 cmd/htmltox/main.go
+GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAGS" -o build/htmltox-darwin-arm64 cmd/htmltox/main.go
